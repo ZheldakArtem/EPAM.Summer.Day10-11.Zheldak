7f437e15bcb0266e961bab7e7faafefb0287ca72
ZheldakArtem <artemzheldak1995@gmail.com>
2016-07-20 17:36:00 +0300

    Add Fibonacci

---
+++ EPAM.Summer.Day10-11.Zheldak/EPAM.Summer.Day10-11.Zheldak/Fibonacci.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+
+{
+    public static class Fibonacci
+    {
+        /// <summary>
+        /// The method get fibonacci numbers
+        /// </summary>
+        /// <param name="count">Amount of numbers</param>
+        /// <returns>Fibonacci numbers</returns>
+        public static IEnumerable<int> GetFibonacci(int count)
+        {
+            int x = 0;
+            int y = 1;
+            int temp = 1;
+            yield return temp;
+            temp = 0;
+            for (int i = 1; i < count; i++)
+            {
+                temp = x + y;
+                x = y;
+                y = temp;
+                yield return temp;
+            }
+        }
+    }
+}
---
+++ EPAM.Summer.Day10-11.Zheldak/Task1.Test/FibonacciTests.cs
@@ -0,0 +1,25 @@
+﻿using Task1;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Test
+{
+    [TestFixture]
+    public class FibonacciTests
+    {
+        [TestCase(5, new int[] { 1, 1, 2, 3, 5 })]
+        [TestCase(1, new int[] { 1 })]
+        [TestCase(6, new int[] { 1, 1, 2, 3, 5, 8 })]
+        public void GetFibonacciNumbersTest(int count, IEnumerable<int> fibonacciNumbers)
+        {
+            var result = Fibonacci.GetFibonacci(count).ToArray();
+
+            CollectionAssert.AreEqual(fibonacciNumbers.ToArray(), result);
+        }
+
+    }
+}
