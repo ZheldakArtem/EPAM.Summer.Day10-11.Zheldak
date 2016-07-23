d0f3c98bc38ad7f71391fe4f67876402dc0cb9d5
ZheldakArtem <artemzheldak1995@gmail.com>
2016-07-23 16:54:47 +0300

    Add test

--- EPAM.Summer.Day10-11.Zheldak/Task4.Test/BinaryTreeTest.cs
+++ EPAM.Summer.Day10-11.Zheldak/Task4.Test/BinaryTreeTest.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using Task4;
+using Task_Book;
 
 namespace Task4.Test
 {
@@ -28,7 +29,7 @@ namespace Task4.Test
         }
 
         [TestCaseSource(typeof(DataSource), nameof(DataSource.IntDataComparer))]
-        public void ThreeWithIntComparer(int[] arrayCollection, IComparer<int> comparerInt,int[] resut)
+        public void ThreeWithIntComparer(int[] arrayCollection, IComparer<int> comparerInt, int[] resut)
         {
             BinaryTree<int> tree = new BinaryTree<int>(arrayCollection, comparerInt);
             var enumeratorTree = tree.GetEnumerator();
@@ -40,12 +41,17 @@ namespace Task4.Test
             }
         }
 
-       
-
-        [Test]
-        public void ThreeWithStringComparer(string[] arrayCollection, IComparer<string> comparerString)
+        [TestCaseSource(typeof(DataSource), nameof(DataSource.DataBook))]
+        public void ThreeWithBookPreorder(Book[] array, Book[] result)
         {
-            BinaryTree<string> tree = new BinaryTree<string>(arrayCollection, comparerString);
+            BinaryTree<Book> tree = new BinaryTree<Book>(array);
+
+            var enumeratorBook = tree.GetEnumerator();
+            var enumeratorResult = result.GetEnumerator();
+            while (enumeratorBook.MoveNext() && enumeratorResult.MoveNext())
+            {
+                Assert.AreEqual(enumeratorResult.Current, enumeratorBook.Current);
+            }
         }
     }
 
@@ -58,9 +64,33 @@ namespace Task4.Test
 
         public static IEnumerable IntDataComparer
         {
-            get { yield return new TestCaseData(new int[] {11111, 11, 1111111111, 1, 1111, 11111111, -111111111},new IntComparer(),new int[] {11111,11,1,1111, 1111111111, 11111111, -111111111}); }
-        } 
+            get { yield return new TestCaseData(new int[] { 11111, 11, 1111111111, 1, 1111, 11111111, -111111111 }, new IntComparer(), new int[] { 11111, 11, 1, 1111, 1111111111, 11111111, -111111111 }); }
+        }
 
+        public static IEnumerable DataBook
+        {
+            get
+            {
+                yield return new TestCaseData(new Book[]
+          {
+                new Book("AAA","AAA","AAA",111,333),
+                new Book("DDD","DDD","DDD",444,444),
+                new Book("EEE","EEE","EEE",555,555),
+                new Book("BBB","BBB","BBB",222,222),
+                new Book("CCC","CCC","CCC",333,111)
+
+
+          },
+          new Book[]
+          {
+                new Book("AAA","AAA","AAA",111,333),
+                new Book("BBB","BBB","BBB",222,222),
+                new Book("CCC","CCC","CCC",333,111),
+                new Book("DDD","DDD","DDD",444,444),
+                new Book("EEE","EEE","EEE",555,555)
+          });
+            }
+        }
     }
 }
 
